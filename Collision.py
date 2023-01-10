bc295ad7353cd65d42167e124f48940be3dd35c5
Legendary <99966559+Legendary3995@users.noreply.github.com>
2023-01-10 21:21:56 +0530

    Update Collision.py

--- Collision.py
+++ Collision.py
@@ -3,8 +3,9 @@ import time
 import pygame
 import random
 from os import system
-frameRate = 100
+frameRate = 200
 collisions = []
+dt = 1/200
 class obj:
     screen_size = [800, 800]
     def __init__(self, mass, center, radius, color = (0, 0, 0), acceleration = 0, velx = 0, vely = 0, e = 1, number = None):
@@ -17,36 +18,45 @@ class obj:
         self.a = acceleration
         self.e = e
         self.number = number
+        self.stop = None
     
     def display(self):
-        pygame.draw.circle(screen, self.color, (round(self.center[0]),round(self.center[1])), self.radius)
+        try:
+            pygame.draw.circle(screen, self.color, (round(self.center[0]),round(self.center[1])), self.radius)
+        except:
+            print(self.color)
     
     def update(self, dt):
         if abs(self.x) > 10000:
             self.x = (abs(self.x)/(self.x))*1000
         if abs(self.y) > 10000:
             self.y = (abs(self.y)/(self.y))*1000
-        self.center[0] += self.x *dt
-        self.center[1] += self.y *dt
-        if self.center[0] > obj.screen_size[0]-self.radius:
-            self.center[0] -= 2*(self.center[0]-self.screen_size[0]+self.radius)
-            self.x *= -1
-        elif self.center[0] < self.radius:
-            self.center[0] += 2*(self.radius-self.center[0])
-            self.x *= -1
+        if self.stop != True:
+            self.center[0] += self.x *dt
+            self.center[1] += self.y *dt
+            if self.center[0] > obj.screen_size[0]-self.radius:
+                self.center[0] -= 2*(self.center[0]-self.screen_size[0]+self.radius)
+                self.x *= -1
+            elif self.center[0] < self.radius:
+                self.center[0] += 2*(self.radius-self.center[0])
+                self.x *= -1
 
-        if self.center[1] > obj.screen_size[1]-self.radius:
-            self.center[1] -= 2*(self.center[1]-self.screen_size[1]+self.radius)
-            self.y *= -1
-        elif self.center[1] < self.radius:
-            self.center[1] += 2*(self.radius-self.center[1])
-            self.y *= -1
+            if self.center[1] > obj.screen_size[1]-self.radius:
+                self.center[1] -= 2*(self.center[1]-self.screen_size[1]+self.radius)
+                self.y *= -1
+            elif self.center[1] < self.radius:
+                self.center[1] += 2*(self.radius-self.center[1])
+                self.y *= -1
+            if self.stop == False:
+                self.stop = None
+        else:
+            self.stop = False
             
 
     def friction(self, gravity, mu):
         pass
     
-    def collision(self, group:list, collisions:list):
+    def collision(self, group:list, collisions:list, dt):
         for i in group:
             if [self.number, i.number] not in collisions:
                 if (i.center[0] - self.center[0]) <= i.radius + self.radius:
@@ -58,8 +68,42 @@ class obj:
                             ]
                             self.x, i.x = final_vels[0][0], final_vels[0][1]
                             self.y, i.y = final_vels[1][0], final_vels[1][1]
+
+                            # self.color = [abs(self.color[0]-i.color[0]), abs(self.color[1]-i.color[1]), abs(self.color[2]-i.color[2])]
+                            # i.color = [abs(i.color[0]-self.color[0]), abs(i.color[1]-self.color[1]), abs(i.color[2]-self.color[2])]
+
+                            self.update(dt *2)
+                            i.update(dt *2)
+
+                            if self.stop == None:
+                                self.stop = True
+                            if i.stop == None:
+                                i.stop = True
+                                
+                            
+                            # Resolving the ball stucking within each other issue from line 85-101, or next 17 lines...
+                            dist = distance(i.center, self.center)
+                            if dist < i.radius + self.radius:
+                                if i.center[0]-self.center[0] == 0:
+                                    i.center[1] += sign(i.center[1] - self.center[1]) * abs(i.radius+self.radius-dist)/2
+                                    self.center[1] += sign(self.center[1] - i.center[1]) * abs(i.radius+self.radius-dist)/2
+                                elif i.center[1]-self.center[1] == 0:
+                                    i.center[0] += sign(i.center[0] - self.center[0]) * abs(i.radius+self.radius-dist)/2
+                                    self.center[0] += sign(self.center[0] - i.center[0]) * abs(i.radius+self.radius-dist)/2
+                                else:
+                                    pass
+                                    incHyp = (i.radius+self.radius-dist)/2
+                                    incx = (incHyp/dist)*(abs(i.center[0] - self.center[0]))
+                                    incy = (incHyp/dist)*(abs(i.center[1] - self.center[1]))
+                                    i.center[0] += -incx* sign(i.center[0] - self.center[0])
+                                    self.center[0] += -incx* sign(self.center[0] - i.center[0])
+                                    i.center[1] += -incy *sign(i.center[0] - self.center[0])
+                                    self.center[1] += -incy *sign(self.center[0] - i.center[0])
                             return [[self.number, i.number], [i.number, self.number]]
         return []
+
+def sign(num):
+    return 1 if num > 0 else -1
 
 def exchange_vel(ivel1, ivel2, m1, m2, e1, e2):
     fvel1 = (1/(m1+m2))*((ivel1*(m1-e1*m2)) + (ivel2*(1+e1)*m2))
@@ -72,14 +116,9 @@ def distance(point1,point2):
 
 
 if __name__ == '__main__':
-    balls = [
-        obj(20, (200,200), 50, (200,0,200), 0, 60*5, 60*5, 1, 0),
-        obj(18, (300,300), 45, (0,200,200), 0, 60*5, 70*5, 1, 1),
-        obj(16, (300,400), 40, (200,200,0), 0, 40*5, 70*5, 1, 2),
-        obj(14, (300,600), 35, (200,0,0), 0, 90*5, 20*5, 1, 3),
-        obj(12, (300,700), 30, (0,200,0), 0, 40*5, 80*5, 1, 4),
-        obj(10, (400,400), 25, (0,0,200), 0, 70*5, 80*5, 1, 5)
-    ]
+    balls = []
+    for i in range(100):
+        balls.append(obj(10+int(random.random()*30), (40 + int(random.random()*600),40 + int(random.random()*600)), 20, (int(random.random()*200)+56,int(random.random()*200)+56,int(random.random()*200)+56), 0, int((0.5-random.random())*500), int((0.5-random.random())*500), 1, i))
     pygame.init()
     screen = pygame.display.set_mode((800,800))
     pygame.display.set_caption("Collision")
@@ -92,16 +131,16 @@ if __name__ == '__main__':
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+
+        #code
+        screen.fill((150,150,150))
+        for i in balls:
+            collisions.extend(i.collision([objs for objs in balls if objs.number != i.number], collisions, dt))
+            i.update(dt)
+            i.display()
+        pygame.display.update()
         endTime = time.time()
         dt = endTime-initTime
         initTime = endTime
         if dt != 0: frameRate = 1/dt
         else: frameRate = 1000
-
-        #code
-        screen.fill((150,150,150))
-        for i in balls:
-            collisions.extend(i.collision([objs for objs in balls if objs.number != i.number], collisions))
-            i.update(dt)
-            i.display()
-        pygame.display.update()
